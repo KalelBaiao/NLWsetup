835729284fcea2a7a0216d9f7bb0a79d200a6f32
KalelBaiao <106087364+KalelBaiao@users.noreply.github.com>
2023-01-17 19:59:32 -0400

    .

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,20 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 import { Habit } from "./components/Habit"
 
 function App() {
   return (
     <Habit />
+=======
+function App() {
+  return (
+    <h1>Hello World</h1>
+>>>>>>> parent of f41c707 (.)
+=======
+function App() {
+  return (
+    <h1>Hello World</h1>
+>>>>>>> parent of f41c707 (.)
   )
 }
 
